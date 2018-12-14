09849a004b758a90cac9b28e6cf3f11b6e7f7954
Jchandler01 <43756533+Jchandler01@users.noreply.github.com>
2018-12-14 10:06:45 -0500

    Add files via upload

---
+++ Lesson8/problem1.py
@@ -0,0 +1,19 @@
+import time
+
+age = input('What is you age today? ')
+age = int(age)
+print()
+
+total = ((79 - age) + 2018)
+
+total = str(total)
+
+
+print('...thinking...')
+
+print('...thinking...')
+
+print('...thinking...')
+
+print('You will die in the year ' + total)
+
